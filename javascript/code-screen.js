82362992ae9955edb3f307d8aeb139bf31d199e3
Stella <dpthf24@gmail.com>
2022-02-19 01:11:02 +0900

    Create code-screen.js

---
+++ javascript/code-screen.js
@@ -0,0 +1,27 @@
+const codeBtn = document.getElementById("code-enter");
+const nameBtn = document.getElementById("name-enter");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function firstPage(event) {
+  event.preventDefault();
+  const code = document.getElementById("code-input");
+  const codeNum = code.value;
+  const pageOne = document.getElementById("first-page");
+  const pageTwo = document.getElementById("second-page");
+  if (codeNum === "1") {
+    pageOne.classList.add(HIDDEN_CLASSNAME);
+    pageTwo.classList.remove(HIDDEN_CLASSNAME);
+  } else if (codeNum === "") {
+    alert("코드를 입력해주세요!");
+  } else {
+    alert("이미 사용했거나 유효하지 않은 코드입니다");
+  }
+}
+
+function secondPage(event) {
+  event.preventDefault();
+}
+
+codeBtn.addEventListener("click", firstPage);
+nameBtn.addEventListener("click", secondPage);
