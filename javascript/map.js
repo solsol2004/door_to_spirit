7cbf77273e39ba13875ef581ced514953aaa47c6
Stella <dpthf24@gmail.com>
2022-04-29 04:41:14 +0900

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,27 +1,37 @@
 var HOME_PATH = window.HOME_PATH || ".";
-
-var place = new naver.maps.LatLng(37.557687, 126.976935),
+var cityhall = new naver.maps.LatLng(37.557687, 126.976935),
   map = new naver.maps.Map("mission-map", {
-    center: place.destinationPoint(0, 500),
+    center: cityhall,
     zoom: 15,
   }),
   marker = new naver.maps.Marker({
     map: map,
-    position: place,
+    position: cityhall,
   });
 
 var contentString = [
-  '<div class="map-font">',
-  "   <span>라라핏 필라테스</span>",
-  '       <img src="assets/img/logo-vertical.png" width="23" height="23" class="thumb" /><br />',
-  "   <p>서울 중랑구 동일로 678-1 / 2층<br /><br />",
-  "       010-6460-3363<br />",
+  '<div class="iw_inner">',
+  "   <h3>서울특별시청</h3>",
+  "   <p>서울특별시 중구 태평로1가 31 | 서울특별시 중구 세종대로 110 서울특별시청<br />",
+  '       <img src="' +
+    HOME_PATH +
+    '/img/example/hi-seoul.jpg" width="55" height="55" alt="서울시청" class="thumb" /><br />',
+  "       02-120 | 공공,사회기관 &gt; 특별,광역시청<br />",
+  '       <a href="http://www.seoul.go.kr" target="_blank">www.seoul.go.kr/</a>',
   "   </p>",
   "</div>",
 ].join("");
 
 var infowindow = new naver.maps.InfoWindow({
   content: contentString,
+  maxWidth: 140,
+  backgroundColor: "#eee",
+  borderColor: "#2db400",
+  borderWidth: 5,
+  anchorSize: new naver.maps.Size(30, 30),
+  anchorSkew: true,
+  anchorColor: "#eee",
+  pixelOffset: new naver.maps.Point(20, -20),
 });
 
 naver.maps.Event.addListener(marker, "click", function (e) {
@@ -31,5 +41,3 @@ naver.maps.Event.addListener(marker, "click", function (e) {
     infowindow.open(map, marker);
   }
 });
-
-infowindow.open(map, marker);
