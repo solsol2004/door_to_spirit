538974f7ef8621233ba580b94a987ab35f589807
Stella <dpthf24@gmail.com>
2022-04-29 04:37:31 +0900

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -11,9 +11,12 @@ var place = new naver.maps.LatLng(37.557687, 126.976935),
   });
 
 var contentString = [
-  '<div class="map-font">',
-  "   <span>1번 문제 장소</span>",
-  "   <p>중구 퇴계로 37-2</p>",
+  '<div class=" ">',
+  "   <span>라라핏 필라테스</span>",
+  '       <img src="assets/img/logo-vertical.png" width="23" height="23" class="thumb" /><br />',
+  "   <p>서울 중랑구 동일로 678-1 / 2층<br /><br />",
+  "       010-6460-3363<br />",
+  "   </p>",
   "</div>",
 ].join("");
 
