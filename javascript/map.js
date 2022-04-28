5ff2678def483a4fa75cb9517f822adff0aae160
Stella <dpthf24@gmail.com>
2022-04-29 04:45:17 +0900

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,23 +1,18 @@
 var HOME_PATH = window.HOME_PATH || ".";
-var cityhall = new naver.maps.LatLng(37.557687, 126.976935),
+var missonSpot = new naver.maps.LatLng(37.557687, 126.976935),
   map = new naver.maps.Map("mission-map", {
-    center: cityhall,
+    center: missonSpot,
     zoom: 15,
   }),
   marker = new naver.maps.Marker({
     map: map,
-    position: cityhall,
+    position: missonSpot,
   });
 
 var contentString = [
   '<div class="iw_inner">',
-  "   <h3>서울특별시청</h3>",
-  "   <p>서울특별시 중구 태평로1가 31 | 서울특별시 중구 세종대로 110 서울특별시청<br />",
-  '       <img src="' +
-    HOME_PATH +
-    '/img/example/hi-seoul.jpg" width="55" height="55" alt="서울시청" class="thumb" /><br />',
-  "       02-120 | 공공,사회기관 &gt; 특별,광역시청<br />",
-  '       <a href="http://www.seoul.go.kr" target="_blank">www.seoul.go.kr/</a>',
+  "   <h3>1번 문제 장소</h3>",
+  "   <p>중구 퇴계로 37-2<br />",
   "   </p>",
   "</div>",
 ].join("");
@@ -25,13 +20,13 @@ var contentString = [
 var infowindow = new naver.maps.InfoWindow({
   content: contentString,
   maxWidth: 140,
-  backgroundColor: "#eee",
-  borderColor: "#2db400",
+  backgroundColor: "white",
+  borderColor: "#ffb005e6",
   borderWidth: 5,
   anchorSize: new naver.maps.Size(30, 30),
   anchorSkew: true,
-  anchorColor: "#eee",
-  pixelOffset: new naver.maps.Point(20, -20),
+  anchorColor: "white",
+  pixelOffset: new naver.maps.Point(20, -10),
 });
 
 naver.maps.Event.addListener(marker, "click", function (e) {
