b913701fcd58cfdfcfe73f7442eb69313be3d9ce
Stella <77248703+solsol2004@users.noreply.github.com>
2022-05-03 03:46:16 +0900

    Update quiz.js

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -10,6 +10,47 @@ const storage = firebase.storage();
 let lat = null;
 let long = null;
 
+function map(){
+  var HOME_PATH = window.HOME_PATH || ".";
+var missonSpot = new naver.maps.LatLng(lat, long),
+  map = new naver.maps.Map("mission-map", {
+    center: missonSpot,
+    zoom: 16,
+  }),
+  marker = new naver.maps.Marker({
+    map: map,
+    position: missonSpot,
+  });
+var contentString = [
+  '<div class="iw_inner">',
+  '<h3 class="map-title">',
+  "   1번 문제 장소</h3>",
+  '<p class="map-description">',
+  "   📍중구 퇴계로 37-2",
+  "   </p>",
+  "</div>",
+].join("");
+var infowindow = new naver.maps.InfoWindow({
+  content: contentString,
+  maxWidth: 140,
+  borderColor: "#606060e6",
+  borderWidth: 1,
+  borderStyle: "double",
+  backgroundColor: "white",
+  padding: 3,
+  anchorColor: "white",
+  pixelOffset: new naver.maps.Point(20, -10),
+});
+naver.maps.Event.addListener(marker, "click", function (e) {
+  if (infowindow.getMap()) {
+    infowindow.close();
+  } else {
+    infowindow.open(map, marker);
+  }
+});
+}
+
+
 db.collection("quiz")
   .doc(chapter.toString())
   .get()
@@ -65,42 +106,4 @@ db.collection("quiz")
   });
 
 
-function map(){
-  var HOME_PATH = window.HOME_PATH || ".";
-var missonSpot = new naver.maps.LatLng(lat, long),
-  map = new naver.maps.Map("mission-map", {
-    center: missonSpot,
-    zoom: 16,
-  }),
-  marker = new naver.maps.Marker({
-    map: map,
-    position: missonSpot,
-  });
-var contentString = [
-  '<div class="iw_inner">',
-  '<h3 class="map-title">',
-  "   </h3>",
-  '<p class="map-description">',
-  "   </p>",
-  "</div>",
-].join("");
-var infowindow = new naver.maps.InfoWindow({
-  content: contentString,
-  maxWidth: 140,
-  borderColor: "#606060e6",
-  borderWidth: 1,
-  borderStyle: "double",
-  backgroundColor: "white",
-  padding: 3,
-  anchorColor: "white",
-  pixelOffset: new naver.maps.Point(20, -10),
-});
-naver.maps.Event.addListener(marker, "click", function (e) {
-  if (infowindow.getMap()) {
-    infowindow.close();
-  } else {
-    infowindow.open(map, marker);
-  }
-});
-}
 
