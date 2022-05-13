8380faf0a938e58d1194ab88b7bcdfd527ced229
Stella <77248703+solsol2004@users.noreply.github.com>
2022-05-13 19:04:21 +0900

    update

--- javascript/index.js
+++ javascript/index.js
@@ -1,73 +1,47 @@
-let codeForm = document.getElementById("code-bottom");
-let nameForm = document.getElementById("name-bottom");
-const myName = document.getElementById("name-input");
 let codeEntered = localStorage.getItem("code-entered");
-
-const HIDDEN_CLASSNAME = "hidden";
 
 //Firebase//
 const db = firebase.firestore();
 const storage = firebase.storage();
 
-//Firebase에 있는 코드를 만료하기 위해 사용하는 키//
-let userCodeEntered = localStorage.getItem("user-code");
 
 
 if (codeEntered === "true") {
   $(".resume-test").show();
   $(".code-already-entered").show();
-  $("#code-bottom").hide();}
+  $(".code-bottom-page").hide();
+}
 
-function firstPage(event) {
-  event.preventDefault();
-  const code = document.getElementById("code-input");
-  const codeNum = code.value;
-  const pageOne = document.getElementById("first-page");
-  const pageTwo = document.getElementById("second-page");
-  if (codeNum === "1") {
-    pageOne.classList.add(HIDDEN_CLASSNAME);
-    pageTwo.classList.remove(HIDDEN_CLASSNAME);
-    localStorage.setItem("used-hint", 0);
-    localStorage.setItem("chapter", 1);
-    localStorage.setItem("code-entered", true);
-  } else if (codeNum === "") {
 
-  } else {
-    //alert("이미 사용했거나 유효하지 않은 코드입니다");
-    code.value = "";
-  }
-
-  db.collection("questCode").where("Random", "==", codeNum)
+$("#enter-the-test").click(function () {
+    db.collection("questCode").where("Random", "==", $(".code-input").val())
     .get()
     .then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
-          // doc.data() is never undefined for query doc snapshots
+        if(doc.data().Used === "false"){
           localStorage.setItem("user-code", doc.id);
-      });
+          localStorage.setItem("used-hint", 0);
+          localStorage.setItem("chapter", 1);
+          localStorage.setItem("code-entered", true);
+          $("#first-page").hide();
+          $("#second-page").show();
+        } else if (codeNum === ""){}
+        else {
+          alert("이미 사용한 코드입니다");
+          codeNum = "";
+        }});
   })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
+    .catch(function() {
+      alert("이미 사용했거나 유효하지 않은 코드입니다");
+      codeNum = "";
     });
-
-    
-}
+  });
 
 
-function secondPage(event) {
-  event.preventDefault();
-  const username = myName.value;
-  if (username === "") {
-    alert("이름을 입력해주세요");
-  } else {
-    localStorage.setItem("username", username);
-    nameForm.submit();
-  }
-}
-
-if (codeForm !== null) {
-  codeForm.addEventListener("submit", firstPage);
-}
-
-if (nameForm !== null) {
-  nameForm.addEventListener("submit", secondPage);
-}
+  $(".name-enter").click(function () {
+    if ($(".name-input").val()==="") {
+      alert("이름을 입력해주세요");
+    } else {
+      localStorage.setItem("username", $(".name-input").val());
+      window.location.href = "intro.html";}
+  })
--- javascript/intro.js
+++ javascript/intro.js
@@ -1,39 +1,21 @@
-const pageThree = document.getElementById("third-page");
-const pageFour = document.getElementById("page-four");
-const pageFive = document.getElementById("fifth-page");
-const haechiForm = document.getElementById("gotohaechi");
-let yes = document.getElementById("materials-yes");
-const fourBtn = document.getElementById("fourBtn");
-const realUsername = document.getElementById("username-change");
-
 const savedUsername = localStorage.getItem("username");
+
+//Firebase//
+const db = firebase.firestore();
+const storage = firebase.storage();
 
 //Firebase에 있는 코드를 만료하기 위해 사용하는 키//
 let userCodeEntered = localStorage.getItem("user-code");
 
-realUsername.innerText = `${savedUsername}님 안녕하세요!`;
 
-function thirdPage(event) {
-  event.preventDefault();
-  pageThree.classList.add("hidden");
-  pageFour.classList.remove("hidden");
-  realUsername.innerText = `${savedUsername}님 안녕하세요!`;
-  db.collection("questCode")
+$("#materials-yes").click(function () {
+    $("#third-page").hide();
+    $("#page-four").show();
+    $("#username-change").text(`${savedUsername}님 안녕하세요!`);
+    db.collection("questCode")
     .doc(userCodeEntered)
     .update({
       Used: "true"
     });
-}
+  })
 
-function forthPage(event) {
-  event.preventDefault();
-  haechiForm.submit();
-}
-
-if (yes !== null) {
-  yes.addEventListener("click", thirdPage);
-}
-
-if (haechiForm !== null) {
-  haechiForm.addEventListener("submit", forthPage);
-}
