b583e8f11bf64d3539e23a024625d8f25b723c5b
Stella <77248703+solsol2004@users.noreply.github.com>
2022-05-24 17:07:16 +0900

    update

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -204,13 +204,15 @@ db.collection("quiz")
       //정답 틀릴 때 //
       else {
       $(".wrong-answer").show();
+      $(".wrong-answer-close").show();
       $(".nav-bar").css('opacity', 0.2);
       $(".quiz").css('opacity', 0.2);}
 
       });
 
-      $(".wrong-answer").click(function () {
+      $(".wrong-answer-close").click(function () {
         $(".wrong-answer").hide();
+        $(".wrong-answer-close").hide();
       $(".nav-bar").css('opacity', 1);
       $(".quiz").css('opacity', 1)});
 
